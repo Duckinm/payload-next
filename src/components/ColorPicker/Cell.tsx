0c30d6601e20035cb2685e2ff3f5d9591627f95b
Amyse <>
2021-08-13 14:11:21 +0700

    hope it works!

--- src/components/ColorPicker/Cell.tsx
+++ src/components/ColorPicker/Cell.tsx
@@ -1,6 +1,16 @@
-import { Props } from 'payload/components/views/Cell'
+import { Field, SanitizedCollectionConfig } from 'payload/types'
 import React from 'react'
 import './styles.scss'
+
+type Props = {
+  field: Field
+  colIndex: number
+  collection: SanitizedCollectionConfig
+  cellData: unknown
+  rowData: {
+    [path: string]: unknown
+  }
+}
 
 const Cell: React.FC<Props> = (props) => {
   const { cellData } = props
