eca3876d37c281289c8d013238bdd9ee84b11263
Amyse <>
2021-08-14 05:41:58 +0700

    :ambulance: :ambulance: fetch Layout

--- src/components/Layouts/Layout.tsx
+++ src/components/Layouts/Layout.tsx
@@ -1,9 +1,10 @@
-// import { useRouter } from 'next/router'
+import { useRouter } from 'next/router'
 import React, { ReactNode } from 'react'
-// import Footer from 'src/components/Layouts/Footer'
-// import Navbar from 'src/components/Layouts/Navbar'
-// import GlobalCustomAlert from 'src/components/Toast'
-// import useSWR from 'swr'
+import Footer from 'src/components/Layouts/Footer'
+import Navbar from 'src/components/Layouts/Navbar'
+import GlobalCustomAlert from 'src/components/Toast'
+import { fetcher } from 'src/utilities/fetcher'
+import useSWR from 'swr'
 
 type Props = {
   title?: string
@@ -11,24 +12,22 @@ type Props = {
 }
 
 const Layout = (props: Props) => {
-  // const { children } = props
-  // const { locale } = useRouter()
+  const { children } = props
+  const { locale } = useRouter()
 
-  // const { data: menu } = useSWR('/api/globals/menu?locale=' + locale)
-  // const { data: contacts } = useSWR('/api/globals/contacts')
+  const { data: menu } = useSWR('/api/globals/menu?locale=' + locale, fetcher)
+  const { data: contacts } = useSWR('/api/globals/contacts', fetcher)
 
-  // if (!menu || !contacts) return <div>Loading...</div>
+  if (!menu || !contacts) return <div>Loading...</div>
 
-  return <div></div>
-
-  // return (
-  //   <>
-  //     <GlobalCustomAlert />
-  //     <Navbar header={menu} />
-  //     {children}
-  //     <Footer footer={menu} contacts={contacts} />
-  //   </>
-  // )
+  return (
+    <>
+      <GlobalCustomAlert />
+      <Navbar header={menu} />
+      {children}
+      <Footer footer={menu} contacts={contacts} />
+    </>
+  )
 }
 
 export default Layout
